64262a44948384a1c4c8d1d4ebd70a70685a8d6e
zakria shakoor <zakria.shakoor646@gmail.com>
2021-11-19 07:06:49 -0800

    added few lines

--- testing.r
+++ testing.r
@@ -1,2 +1,10 @@
 # this is from git hub
 print("hellow from git")
+
+name = "zakria"
+full_name = 'zakria shakoor'
+last_name = 'abdul shakoor'
+print(version)
+print(name)
+print(full_name)
+print(last_name)
